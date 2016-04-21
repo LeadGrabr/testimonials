0590f205cdfe68d9adf7e18d1b680933e3410112
Benjamin Tatum <bentatum@me.com>
2016-04-21 10:23:50 -0400

    0.0.4

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -4,63 +4,67 @@ import { Base, Heading, Text } from 'rebass'
 
 const imgSize = 100
 
-const Testimonial = ({ img, name, text }) =>
-    <Flex
-        align="center"
-        column
-        mb={2}
-    >
-        <Box
+const Testimonial = ({ img, name, text, width }, { breakpoints: { small } }) => {
+    const textAlign = width > small ? 'left' : 'center'
+    return (
+        <Flex
+            align="center"
+            column={!width || !(width > small)}
             mb={2}
-            px={2}
         >
-            <Base
-                circle
-                style={{
-                    backgroundImage: `url('${img}')`,
-                    backgroundSize: 'cover',
-                    height: imgSize,
-                    width: imgSize
-                }}
-            />
-        </Box>
-        <Box px={2}>
-            <Heading
-                level={4}
-                style={{
-                    textAlign: 'center'
-                }}
+            <Box
+                mb={2}
+                px={2}
             >
-                {name}
-            </Heading>
-            <Text
-                style={{
-                    textAlign: 'center'
-                }}
-            >
-                {text}
-            </Text>
-        </Box>
-    </Flex>
+                <Base
+                    circle
+                    style={{
+                        backgroundImage: `url('${img}')`,
+                        backgroundSize: 'cover',
+                        height: imgSize,
+                        width: imgSize
+                    }}
+                />
+            </Box>
+            <Box px={2}>
+                <Heading
+                    level={4}
+                    style={{ textAlign }}
+                >
+                    {name}
+                </Heading>
+                <Text style={{ textAlign }}>
+                    {text}
+                </Text>
+            </Box>
+        </Flex>
+    )
+}
 
 Testimonial.propTypes = {
     img: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
+    width: PropTypes.number
 }
 
-const Testimonials = ({ testimonials }) =>
+Testimonial.contextTypes = {
+    breakpoints: PropTypes.object.isRequired
+}
+
+const Testimonials = ({ testimonials, ...props }) =>
     <Flex wrap>
-        {testimonials.map((props, key) =>
+        {testimonials.map((testimonial, key) =>
             <Testimonial
                 key={key}
                 {...props}
+                {...testimonial}
             />
         )}
     </Flex>
 
 Testimonials.propTypes = {
-    testimonials: PropTypes.array.isRequired
+    testimonials: PropTypes.arrayOf(PropTypes.object).isRequired
 }
 
 export default Testimonials
